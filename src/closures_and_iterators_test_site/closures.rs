11e2d726efdca3bbb724575016a6a784727fce7b
Murrelken <danilbutin@gmail.com>
2023-08-15 01:09:58 +0200

    clarify closures functions with comments

--- src/closures_and_iterators_test_site/closures.rs
+++ src/closures_and_iterators_test_site/closures.rs
@@ -1,10 +1,22 @@
-/// Actual return type is ```fn(&i32) -> bool```, and it implements ```Fn(&i32) -> bool```
-///
-/// Returning ```is_even``` in it's current implementation is equal to returning ```|x| x % 2 == 0```
+/// Performs partial application to the ```is_modulo_zero```, and returns partial function.
 pub fn get_test_is_even_fn() -> impl Fn(&i32) -> bool {
-    is_even
+    get_is_modulo_zero_partial_by_divider(&2)
 }
 
-fn is_even(x: &i32) -> bool {
-    x % 2 == 0
+/// This function creates a new closure and returns it.
+/// Therefore the return type is no longer ```fn(..) -> ..```, but ```Fn(..) -> ..```
+///
+/// Specifically, it allows to perform partial application to the ```is_modulo_zero``` function.
+///
+/// A closure is something that captures any value from outer scope.
+/// For example, something like ```|x: &i32, d: &i32| x % d == 0```
+/// isn't a closure to the compiler, but rather a function pointer - ```fn```
+fn get_is_modulo_zero_partial_by_divider<'a>(divider: &'a i32) -> impl Fn(&i32) -> bool + 'a {
+    move |x: &i32| is_modulo_zero(x, divider)
+}
+
+/// Type of this function is ```fn(&i32, &i32) -> bool```,
+/// but it also implements ```Fn(&i32, &i32) -> bool```
+fn is_modulo_zero(x: &i32, divider: &i32) -> bool {
+    x % divider == 0
 }
